1b5fc41ba28a4e6f0e64bdb0c27cfcc4dc438045
Aditya Mitra <adityamitra1089@gmail.com>
2023-07-30 09:12:31 -0700

    This is a polynomial regression model that tries predict the salary of employees. It uses sklearn to make the model and pyplot to scatter the data.

--- animaldetect/vgg16.py
+++ animaldetect/vgg16.py
@@ -1,13 +1,15 @@
 import tensorflow as tf
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input, decode_predictions
 from tensorflow.keras.preprocessing import image
+from tensorflow.keras.models import load_model
 import numpy as np
 
 # Load the VGG16 model
-model = VGG16(weights='imagenet')
+# model = VGG16(weights='imagenet')
+model = load_model('vgg16_animals10.keras')
 
 # Load and preprocess an image
-img_path = '/mnt/c/Users/Aditya Mitra/Downloads/Dog1.jpg'  # Replace with the path to your image
+img_path = '/mnt/c/Users/Aditya Mitra/Downloads/Dog2.jpeg'  # Replace with the path to your image
 img = image.load_img(img_path, target_size=(224, 224))  # VGG16 input size is (224, 224)
 x = image.img_to_array(img)
 x = np.expand_dims(x, axis=0)
