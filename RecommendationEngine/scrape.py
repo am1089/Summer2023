c792976acfd073f8dc43cf69728d5492e44df66d
Aditya Mitra <adityamitra1089@gmail.com>
2023-08-29 20:16:12 -0700

    Scrape and save files in parallel
    Problem:
    We are server/session constrained in downloading the files.
    
    Solution:
    Download files in parallel using the Pool mechanism.
    
    Test:
    Tested with 2 threads in parallel.
    
    Test Logs:
    
    Started tt0110912
    Started tt1872181
    Finished tt1872181
    Started tt1990314
    Finished tt1990314
    Started tt1999995
    Finished tt1999995
    Finished tt0110912

--- RecommendationEngine/scrape.py
+++ RecommendationEngine/scrape.py
@@ -1,22 +1,24 @@
 import requests
 from bs4 import BeautifulSoup
+from multiprocessing import Pool
 
 def getLinks(keys):
-    k = keys
+    k = [keys]
     reviewcontainers = {}
     params = {
         'ref_': 'undefined',
         'paginationKey': ''
     }
-    
+    print("Started " + keys)
+
     for id in k:
         url = 'https://www.imdb.com/title/' + id + '/reviews/'
         link = 'https://www.imdb.com/title/' + id + '/reviews/_ajax'
         hdr = {'User-Agent': 'Mozilla/5.0'}
         r = requests.get(url, headers = hdr)
         loadCount = 0
-        
-        while True:    
+
+        while True:
             f1 = open('data/'+id+'-'+str(loadCount), 'wb')
             f1.write(r.content)
             f1.close()
@@ -29,7 +31,11 @@ def getLinks(keys):
                 break
             params['paginationKey'] = pagination_key
             r = requests.get(link,params=params)
+    print("Finished " + keys)
+    return k
 
-movies = ['tt0110912', 'tt1872181']
+movies = ['tt0110912', 'tt1872181', 'tt1990314', 'tt1999995']
 
-getLinks(movies)
+if __name__ == '__main__':
+    threads = Pool(2)
+    threads.map(getLinks, movies)
